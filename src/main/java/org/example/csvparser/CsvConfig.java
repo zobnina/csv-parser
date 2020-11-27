a0f3861eb07d989cdeff50c934bf8221d02c9407
Aleksandra Zobnina <azobnina@neoflex.ru>
2020-11-27 22:15:59 +0300

    config

--- src/main/java/org/example/csvparser/CsvConfig.java
+++ src/main/java/org/example/csvparser/CsvConfig.java
@@ -1,4 +1,31 @@
 package org.example.csvparser;
 
 public class CsvConfig {
+    private final FieldDelimiter fieldDelimiter;
+    private final TextDelimiter textDelimiter;
+    private final boolean isHeaders;
+
+    public CsvConfig() {
+        fieldDelimiter = FieldDelimiter.COMMA;
+        textDelimiter = TextDelimiter.DOUBLE_QUOTE;
+        isHeaders = true;
+    }
+
+    public CsvConfig(FieldDelimiter delimiter, TextDelimiter frame, boolean isHeaders) {
+        this.fieldDelimiter = delimiter;
+        this.textDelimiter = frame;
+        this.isHeaders = isHeaders;
+    }
+
+    public FieldDelimiter getFieldDelimiter() {
+        return fieldDelimiter;
+    }
+
+    public TextDelimiter getTextDelimiter() {
+        return textDelimiter;
+    }
+
+    public boolean isHeaders() {
+        return isHeaders;
+    }
 }
