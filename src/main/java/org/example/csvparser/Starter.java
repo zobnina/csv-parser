d9e93a63f1e0bfb436be3b8fe0d044e2453164bf
Aleksandra Zobnina <azobnina@neoflex.ru>
2020-11-27 22:25:57 +0300

    test

--- src/main/java/org/example/csvparser/Starter.java
+++ src/main/java/org/example/csvparser/Starter.java
@@ -1,7 +1,16 @@
 package org.example.csvparser;
 
 public class Starter {
-    public static void main(String[] args) {
+    private static final String USER_DIR = System.getProperty("user.dir");
 
+    public static void main(String[] args) {
+        String srcFile1 = USER_DIR + "/src/main/resources/srcFiles/1.csv";
+        String outFile1 = USER_DIR + "/src/main/resources/outFiles/1.csv";
+        CsvConfig config = new CsvConfig();
+        CsvReader reader = new CsvReader(config);
+        CsvData csvData = reader.read(srcFile1);
+        config = new CsvConfig(FieldDelimiter.COMMA, TextDelimiter.SINGLE_QUOTE, true);
+        CsvWriter writer = new CsvWriter(config);
+        writer.write(csvData, outFile1);
     }
 }
